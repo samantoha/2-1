9feaa94e1f1918568d3d0ae932127f7de6b28abe
Anton Samolazov <sam_antoha@mail.ru>
2021-05-16 18:03:09 +1000

    first

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int count = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = count + transfer;
+    System.out.println(total);
+    }
+}
